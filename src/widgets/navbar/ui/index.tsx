9c4d5d7c1124bed40401fa69883d75040e66551b
DeDeimos <rodion.tolkachev.03@inbox.ru>
2023-10-14 15:27:03 +0300

    navbar animation + queue page

--- src/widgets/navbar/ui/index.tsx
+++ src/widgets/navbar/ui/index.tsx
@@ -1,17 +1,23 @@
-import styles from "./index.module.scss";
-import Maps from "../../../assets/maps.svg";
-import Queue from "../../../assets/queue.svg";
+import styles from './index.module.scss';
+import Maps from '../../../assets/maps.svg';
+import Queue from '../../../assets/queue.svg';
+import { Link } from 'react-router-dom';
 
 export const Navabar: React.FC = () => {
-    return (
-      <div className={styles.navbar}>
-          <div className={styles.navbarItem}>
-            <img src={Maps} className={styles.navbarItemIcon}/>
-          </div>
-          <div className={styles.navbarItem}>
-            <img src={Queue} className={styles.navbarItemIcon}/>
-          </div>
-      </div>
-    );
+  return (
+    <div className={styles.navbar}>
+      <Link to="/map">
+        <div className={styles.navbarItem}>
+          <img src={Maps} className={styles.navbarItemIcon} />
+          <span className={styles.navbarItemName}>Карты</span>
+        </div>
+      </Link>
+      <Link to="/queue">
+        <div className={styles.navbarItem}>
+          <img src={Queue} className={styles.navbarItemIcon} />
+          <span className={styles.navbarItemName}>Очередь</span>
+        </div>
+      </Link>
+    </div>
+  );
 };
-  
