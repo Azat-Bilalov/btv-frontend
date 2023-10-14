4d80d5d54f788b8f97f2ff6005e9a0f7317b5596
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-10-15 01:22:33 +0300

    add touch handlers

--- src/widgets/info-mobile/ui/index.tsx
+++ src/widgets/info-mobile/ui/index.tsx
@@ -17,15 +17,41 @@ const InfoMobileWidget: React.FC<InfoMobileProps> = ({ children }) => {
   // 3. открыта (отображается и открыта)
   const { selected } = useMapStore();
   const [fullyOpened, setFullyOpened] = React.useState(false);
+  const [touchStart, setTouchStart] = React.useState(0);
 
   const cnInfoMobile = cn(styles.infoMobile, {
     [styles.infoMobileOpened]: selected !== null,
     [styles.infoMobileFullyOpened]: fullyOpened && selected !== null,
   });
 
+  const touchStartHandler = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (e.touches.length === 1) {
+      setTouchStart(e.touches[0].clientY);
+    }
+  };
+
+  const touchMoveHandler = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (e.touches.length === 1) {
+      const currentY = e.touches[0].clientY;
+      const diff = touchStart - currentY;
+
+      if (diff > 0) {
+        setFullyOpened(true);
+      }
+
+      if (diff < 0) {
+        setFullyOpened(false);
+      }
+    }
+  };
+
   return (
     <div className={cnInfoMobile}>
-      <div className={styles.infoMobileTop}>
+      <div
+        className={styles.infoMobileTop}
+        onTouchStart={touchStartHandler}
+        onTouchMove={touchMoveHandler}
+      >
         <div className={styles.infoMobileTopTitle}>Офис</div>
         {fullyOpened ? (
           <div className={styles.infoMobileTopClose}>
